50b7fcf3fdd5e9d766984349f1a986eff75d2d74
Ed Saunders <saunders.ed@gmail.com>
2018-10-25 15:38:22 +0100

    Wrap tests in  adescribe for sensible grouping

--- apps/web_server/test/calculator_test.exs
+++ apps/web_server/test/calculator_test.exs
@@ -3,13 +3,15 @@ defmodule WebServer.CalculatorTest do
 
   alias WebServer.Calculator
 
-  test "returns nil if we aren't given a List" do
-    assert Calculator.add(nil) == nil
-    assert Calculator.add("Hello, this clearly is ridiculous") == nil
-    assert Calculator.add(0) == nil
-  end
+  describe "add/1" do
+    test "returns nil if we aren't given a List" do
+      assert Calculator.add(nil) == nil
+      assert Calculator.add("Hello, this clearly is ridiculous") == nil
+      assert Calculator.add(0) == nil
+    end
 
-  test "returns 0 with an empty list" do
-    assert Calculator.add([]) == 0
+    test "returns 0 with an empty list" do
+      assert Calculator.add([]) == 0
+    end
   end
 end
